b97b1c1e8cf78c208abb420ad5436a3e2230c05b
rdutto <rdutto@kafesistemas.com.ar>
2020-07-07 16:56:04 -0300

    BETA FEAT add .env
    ADD env_replace.sh to setup ngingx and certbot config.

--- nginx-rproxy/assets/entrypoint.sh
+++ nginx-rproxy/assets/entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-# Get certs
-certbot certonly -n -d example.com -d www.example.com \
-  --standalone --preferred-challenges http --email example@yopmail.com --agree-tos --expand
+# # Get certs
+# certbot certonly -n -d example.com -d www.example.com\
+#   --standalone --preferred-challenges http --email DOMAIN_EMAIL --agree-tos --expand
 
-# Kick off cron
-/usr/sbin/crond -f -d 8 &
+# # Kick off cron
+# /usr/sbin/crond -f -d 8 &
 
+/bin/sh /env_replace.sh
 # Start nginx
 /usr/sbin/nginx -g "daemon off;"
